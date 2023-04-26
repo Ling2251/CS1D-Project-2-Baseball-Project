6a9cd06292e8714ead408d4c37a539402e035e3e
Peijun Zhao <pzhao1@saddleback.edu>
2023-04-25 23:02:53 -0700

    added the planntingtrip class

--- planntingtrip.cpp
+++ planntingtrip.cpp
@@ -1,5 +1,6 @@
 #include "planntingtrip.h"
 #include "ui_planntingtrip.h"
+#include "mainwindow.h"
 
 PlanntingTrip::PlanntingTrip(QWidget *parent) :
     QDialog(parent),
@@ -12,3 +13,20 @@ PlanntingTrip::~PlanntingTrip()
 {
     delete ui;
 }
+
+void PlanntingTrip::on_mainPagrButton_clicked()
+{
+    QList<QWidget*> topLevelWidgets = qApp->topLevelWidgets();
+    foreach(QWidget *widget, topLevelWidgets) {
+       QMainWindow *mainWindow = qobject_cast<QMainWindow*>(widget);
+       if (mainWindow) {
+           // Show the main window if it was previously hidden
+           if (!mainWindow->isVisible()) {
+               mainWindow->show();
+           }
+           break;
+       }
+    }
+    hide();
+}
+
