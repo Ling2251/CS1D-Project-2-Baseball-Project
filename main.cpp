adc845dcd5c3cfa84ac8df39d8623b24363bdda8
Peijun Zhao <pzhao1@saddleback.edu>
2023-04-26 18:49:02 -0700

    fix the UI priority

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    //w.show();
-    loginPage login;
-    login.show();
+    w.show();
     return a.exec();
 }
