7725e943d55944242beeb6e10b52905a509c5025
austin2578 <129340028+austin2578@users.noreply.github.com>
2023-05-15 18:39:12 -0700

    Update graphmst.h

--- graphmst.h
+++ graphmst.h
@@ -28,58 +28,7 @@ public:
         void addEdge(string src, string dest, int weight)
         {
             edges.push_back({weight, {src, dest}});
-        }
-
-        // Function to find MST using Kruskal's
-        // MST algorithm
-        int kruskalMST()
-        {
-            int mst_wt = 0; // Initialize result
-
-            // Sort edges in increasing order on basis of cost
-            sort(edges.begin(), edges.end());
-
-            // Extract all cities
-            vector<string> cities;
-            for (const auto& e : edges) {
-                cities.push_back(e.second.first);
-                cities.push_back(e.second.second);
-            }
-            sort(cities.begin(), cities.end());
-            cities.erase(unique(cities.begin(), cities.end()), cities.end());
-
-            // Create disjoint sets
-            DisjointSets ds(cities);
-
-            // Iterate through all sorted edges
-            for (const auto& e : edges)
-            {
-                int weight = e.first;
-                string src = e.second.first;
-                string dest = e.second.second;
-
-                string set_src = ds.find(src);
-                string set_dest = ds.find(dest);
-
-                // Check if the selected edge is creating
-                // a cycle or not (Cycle is created if src
-                // and dest belong to same set)
-                if (set_src != set_dest)
-                {
-                    // Current edge will be in the MST
-                    // so print it with its weight
-                    cout << src << " - " << dest << " : " << weight << endl;
-
-                    // Update MST weight
-                    mst_wt += weight;
-
-                    // Merge two sets
-                    ds.merge(set_src, set_dest);
-                }
-            }
-
-            return mst_wt;
-        }
+        }        
 
     // To represent Disjoint Sets
     struct DisjointSets
@@ -129,6 +78,56 @@ public:
         }
     };
     
+// Function to find MST using Kruskal's
+// MST algorithm
+int kruskalMST()
+{
+    int mst_wt = 0; // Initialize result
+
+    // Sort edges in increasing order on basis of cost
+    sort(edges.begin(), edges.end());
+
+    // Extract all cities
+    vector<string> cities;
+    for (const auto& e : edges) {
+        cities.push_back(e.second.first);
+        cities.push_back(e.second.second);
+     }
+     sort(cities.begin(), cities.end());
+     cities.erase(unique(cities.begin(), cities.end()), cities.end());
+
+      // Create disjoint sets
+      DisjointSets ds(cities);
+
+      // Iterate through all sorted edges
+      for (const auto& e : edges)
+      {
+           int weight = e.first;
+           string src = e.second.first;
+           string dest = e.second.second;
+
+           string set_src = ds.find(src);
+           string set_dest = ds.find(dest);
+
+           // Check if the selected edge is creating
+           // a cycle or not (Cycle is created if src
+           // and dest belong to same set)
+           if (set_src != set_dest)
+           {
+             // Current edge will be in the MST
+             // so print it with its weight
+               cout << src << " - " << dest << " : " << weight << endl;
+
+               // Update MST weight
+               mst_wt += weight;
+
+               // Merge two sets
+                ds.merge(set_src, set_dest);
+           }
+      }
+
+   return mst_wt;
+}    
 
 private slots:
     void on_MSTpushButton_clicked();
